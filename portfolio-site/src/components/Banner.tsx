519fee59b8845821f1a185593ee43bb9e568be26
asea-aranion <82777688+asea-aranion@users.noreply.github.com>
2025-08-14 08:43:35 -0700

    add link to banner

--- portfolio-site/src/components/Banner.tsx
+++ portfolio-site/src/components/Banner.tsx
@@ -2,9 +2,15 @@ import styles from "../css/Banner.module.css";
 
 const Banner = () => {
     return (
-        <>
+        <header className={styles.header}>
             <h1 className={styles.title}>apps by leia spagnola</h1>
-        </>
+            <a
+                className={styles.subtitle}
+                href="https://github.com/asea-aranion"
+                target="blank">
+                asea-aranion on github
+            </a>
+        </header>
     );
 };
 
