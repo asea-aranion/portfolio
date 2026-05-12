3a3399f771f3c24dd46240d80a4746f0c65e5f69
asea-aranion <82777688+asea-aranion@users.noreply.github.com>
2026-05-12 12:04:49 -0400

    add italics, list items, and styling

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,15 +1,19 @@
 import { useMarkdownTyping } from "../mdAnimation";
 import styles from "../css/Intro.module.css";
+import { useState } from "react";
 
-const text = `Welcome to **Leia Spagnola**'s portfolio site (that's me!).\n\n
-	Here's what I've been working on lately:\n
-	- A [cross-platform mobile app](https://github.com/Hack4Impact-UMD/mount-vernon-trail) for Friends of the Mount Vernon Trail volunteers\n
-	- An [iOS packing list app](https://github.com/asea-aranion/Packet) to help you travel stress-free\n
-	- A [VSCode extension](https://github.com/asea-aranion/commitiquette) to notify you when your uncommitted changes are starting to look a bit hefty\n
-	- A [Tidbyt app](https://github.com/tidbyt/community/tree/main/apps/underwaterclock) to bring some oceanic calm to your desk or home`;
+const text = `Hello, dear reader! I'm **Leia**, a rising junior studying _computer science and linguistics_ at the **University of Maryland, College Park**.
+
+Other things I've been working on lately:
+- A [cross-platform mobile app](https://github.com/Hack4Impact-UMD/mount-vernon-trail) for Friends of the Mount Vernon Trail volunteers
+- An [iOS packing list app](https://github.com/asea-aranion/Packet) to help you travel stress-free
+- A [VSCode extension](https://github.com/asea-aranion/commitiquette) to notify you when your uncommitted changes are starting to look a bit hefty
+- A [Tidbyt app](https://github.com/tidbyt/community/tree/main/apps/underwaterclock) to bring some oceanic calm to your desk or home`;
 
 const Intro = () => {
-    const { display } = useMarkdownTyping(text);
+    const [, setDone0] = useState(false);
+
+    const { display } = useMarkdownTyping(text, true, setDone0);
 
     return (
         <div
--- src/mdAnimation.ts
+++ src/mdAnimation.ts
@@ -4,97 +4,169 @@ type MarkdownTypingData = {
     workingText: string;
     displayText: string;
     lastDoubleStar: number | null;
+    lastUnderscore: number | null;
+    lastBullet: number | null;
+    applyListItemIn: number | null;
     applyBoldNext: boolean;
 };
 
-const MS_PER_CHAR = 20;
+export const MS_PER_CHAR = 20;
+
 const doubleStarPattern = /^\*\*/;
+const underscorePattern = /^_/;
+const bulletPattern = /^\n- /;
 const newlinePattern = /^\n/;
 const closingParenPattern = /^\)/;
 const linkPattern = /\[(.*)\]\((.*)$/;
 
-export const useMarkdownTyping = (text: string) => {
+export const useMarkdownTyping = (
+    text: string,
+    readyToStart: boolean,
+    setDone: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
     const [data, setData] = useState<MarkdownTypingData>({
         workingText: text,
         displayText: "",
         lastDoubleStar: null,
+        lastUnderscore: null,
+        lastBullet: null,
+        applyListItemIn: null,
         applyBoldNext: false,
     });
 
     const timer = useRef<number>(undefined);
 
     useEffect(() => {
-        timer.current = setInterval(() => {
-            setData(
-                ({
-                    workingText,
-                    displayText,
-                    lastDoubleStar,
-                    applyBoldNext,
-                }: MarkdownTypingData) => {
-                    const updatedData: MarkdownTypingData = {
+        if (readyToStart) {
+            timer.current = setInterval(() => {
+                setData(
+                    ({
                         workingText,
                         displayText,
                         lastDoubleStar,
+                        lastUnderscore,
+                        lastBullet,
+                        applyListItemIn,
                         applyBoldNext,
-                    };
+                    }: MarkdownTypingData) => {
+                        const updatedData: MarkdownTypingData = {
+                            workingText,
+                            displayText,
+                            lastDoubleStar,
+                            lastUnderscore,
+                            lastBullet,
+                            applyListItemIn,
+                            applyBoldNext,
+                        };
 
-                    if (!workingText) {
-                        clearInterval(timer.current);
-                        return updatedData;
-                    }
-
-                    let newDisplayText = displayText + workingText[0];
-
-                    if (applyBoldNext) {
-                        const textBeforeBold = displayText.substring(
-                            0,
-                            lastDoubleStar!,
-                        );
-                        const boldedText = displayText.substring(
-                            lastDoubleStar! + 2,
-                            displayText.length - 1,
-                        );
-
-                        newDisplayText = `${textBeforeBold}<b>${boldedText}</b>`;
-                        updatedData.lastDoubleStar = null;
-                        updatedData.applyBoldNext = false;
-                    }
-
-                    if (workingText.match(newlinePattern)) {
-                        newDisplayText = displayText + "<br />";
-                    } else if (workingText.match(doubleStarPattern)) {
-                        if (lastDoubleStar != null) {
-                            // close bolded text with tags next iteration
-                            updatedData.applyBoldNext = true;
-                        } else {
-                            // save position
-                            updatedData.lastDoubleStar = displayText.length;
+                        if (!workingText) {
+                            clearInterval(timer.current);
+                            setDone(true);
+                            return updatedData;
                         }
-                    } else if (workingText.match(closingParenPattern)) {
-                        const linkMatch = displayText.match(linkPattern);
-                        if (linkMatch) {
-                            const startOfLink =
-                                displayText.length -
-                                (linkMatch[1].length + linkMatch[2].length + 3);
-                            const textBeforeLink = displayText.substring(
+
+                        let newDisplayText = displayText + workingText[0];
+
+                        if (applyListItemIn != null) {
+                            if (applyListItemIn > 0) {
+                                updatedData.applyListItemIn =
+                                    applyListItemIn - 1;
+                            } else {
+                                const textBeforeListItem =
+                                    displayText.substring(0, lastBullet!);
+
+                                newDisplayText =
+                                    `${textBeforeListItem}<li>` +
+                                    workingText[0];
+                                updatedData.applyListItemIn = null;
+                            }
+                        }
+
+                        if (applyBoldNext) {
+                            const textBeforeBold = displayText.substring(
                                 0,
-                                startOfLink,
+                                lastDoubleStar!,
                             );
-                            newDisplayText = `${textBeforeLink}<a href="${linkMatch[2]}" target="_blank">${linkMatch[1]}</a>`;
+                            const boldedText = displayText.substring(
+                                lastDoubleStar! + 2,
+                                displayText.length - 1,
+                            );
+
+                            newDisplayText = `${textBeforeBold}<b>${boldedText}</b>`;
+                            updatedData.lastDoubleStar = null;
+                            updatedData.applyBoldNext = false;
                         }
-                    }
 
-                    updatedData.displayText = newDisplayText;
-                    updatedData.workingText = workingText.substring(1);
+                        if (workingText.match(bulletPattern)) {
+                            if (lastBullet != null) {
+                                // close previous list item and open a new one
+                                newDisplayText = displayText + "</li>";
+                            } else {
+                                // open list and list item
+                                newDisplayText = displayText + "<ul>";
+                            }
+                            updatedData.applyListItemIn = 2;
+                            updatedData.lastBullet = newDisplayText.length;
+                        } else if (workingText.match(newlinePattern)) {
+                            if (lastBullet != null) {
+                                // close list item and list
+                                // we've already checked that we're not starting a new list item
+                                newDisplayText = displayText + "</li></ul>";
+                            } else {
+                                newDisplayText = displayText + "<br />";
+                            }
+                        } else if (workingText.match(doubleStarPattern)) {
+                            if (lastDoubleStar != null) {
+                                // close bolded text with tags next iteration
+                                updatedData.applyBoldNext = true;
+                            } else {
+                                // save position
+                                updatedData.lastDoubleStar = displayText.length;
+                            }
+                        } else if (workingText.match(underscorePattern)) {
+                            if (lastUnderscore != null) {
+                                // close italicized text with tags
+                                const textBeforeItalics = displayText.substring(
+                                    0,
+                                    lastUnderscore,
+                                );
+                                const italicizedText = displayText.substring(
+                                    lastUnderscore + 1,
+                                );
 
-                    return updatedData;
-                },
-            );
-        }, MS_PER_CHAR);
+                                newDisplayText = `${textBeforeItalics}<i>${italicizedText}</i>`;
+                                updatedData.lastUnderscore = null;
+                            } else {
+                                // save position
+                                updatedData.lastUnderscore = displayText.length;
+                            }
+                        } else if (workingText.match(closingParenPattern)) {
+                            const linkMatch = displayText.match(linkPattern);
+                            if (linkMatch) {
+                                const startOfLink =
+                                    displayText.length -
+                                    (linkMatch[1].length +
+                                        linkMatch[2].length +
+                                        3);
+                                const textBeforeLink = displayText.substring(
+                                    0,
+                                    startOfLink,
+                                );
+                                newDisplayText = `${textBeforeLink}<a href="${linkMatch[2]}" target="_blank">${linkMatch[1]}</a>`;
+                            }
+                        }
+
+                        updatedData.displayText = newDisplayText;
+                        updatedData.workingText = workingText.substring(1);
+
+                        return updatedData;
+                    },
+                );
+            }, MS_PER_CHAR);
+        }
 
         return () => clearInterval(timer.current);
-    }, []);
+    }, [setDone, readyToStart]);
 
     return { display: data.displayText };
 };
